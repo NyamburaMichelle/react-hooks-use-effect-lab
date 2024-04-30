e19435a9b3a967b10bf1bece5c464ee0f60568ee
Michelle <nyamburamichelle400@gmail.com>
2024-04-30 13:49:22 +0300

    completes lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,14 +1,46 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (timeRemaining > 0) {
+        setTimeRemaining(prevTime => prevTime - 1);
+      }else if (prevTime === 1) {
+        onAnswered(false);
+        return 10;
+      }else {
+        setTimeRemaining(10);
+        onAnswered(false)
+
+
+
+
+
+
+
+
+
+        
+      }
+    }, 1000);
+  
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [timeRemaining,onAnswered]);
+  
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
   }
+  setInterval(() => {
+    onAnswered(false);
+  }, 10000);
 
   const { id, prompt, answers, correctIndex } = question;
 
